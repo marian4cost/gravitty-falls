7dd128be85342f719ac4706aad2f08003ed06790
marian4cost <marian4cost@github.com>
2025-12-23 10:55:12 -0300

    inital stylization with css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+import { Grandstander } from "next/font/google";
+
 import { BoxCharacters } from "../components/box_characters";
+
+import styles from "./page.module.css"
+
+//create the font
+const grandstander = Grandstander({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-grandstander"
+})
 
 export default function Home(){
   return(
-    <main>
-      <div>
-        <h1>Gravitty Falls</h1>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1 className={`${styles.h1} ${grandstander.className}`}>Gravitty Falls</h1>
         <h2>Trust no One</h2>
         <button>Explore the mystery</button>
       </div>
-      
+
       <div>
         <BoxCharacters />
       </div>
